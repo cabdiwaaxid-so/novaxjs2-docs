fbfa40217693ac5434698e10e4c79f2c9ce2a52c
Cabdi Waaxid Siciid <158821136+WhoappRoom@users.noreply.github.com>
2025-07-12 20:02:30 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -4,34 +4,65 @@ const fs = require('fs');
 const app = new nova();
 const port = 3000;
 
-app.serveStatic('public')
-
+app.serveStatic('public');
+app.setViewEngine('novax', {
+  viewsPath: path.join(__dirname, '../public/docs')
+})
+const docs = ['about', 'installation', 'features', 'usage', 'api-reference', 'plugin-system', 'file-uploads', 'cors', 'middleware', 'templating', 'route-middleware', 'responses', 'error-handling', 'example', 'projects', 'contact', 'static-files', 'file-configuration']
 app.get('/', (req, res) => {
   app.sendFile(path.join(__dirname, '../public/index.html'), res)
 });
-
+app.get('/docs/:feature', (req, res) => {
+  try {
+    const target = req.params.feature;
+  if(target && docs.includes(target)) {
+    const footer = fs.readFileSync(path.join(__dirname, '../public/footer.html'), 'utf8');
+    const render = app.render(target, {footer: footer });
+    if(render) {
+      return render;
+    } else {
+      res.send('Rendering error please try again')
+    }
+  } else {
+    res.redirect('/404.html')
+  }
+  } catch(err) {
+    res.send(err)
+  }
+})
 app.get('/sitemap', (req, res) => {
-  res.send(`<?xml version="1.0" encoding="UTF-8"?>
+  const baseUrl = `${req.protocol}://${req.headers.host}`;
+  const currentDate = new Date().toISOString();
+  
+  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset
       xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-
-
 <url>
-  <loc>https://novaxjs.vercel.app/</loc>
+  <loc>${baseUrl}/</loc>
   <lastmod>2025-04-24T21:21:19+00:00</lastmod>
-</url>
+</url>`;
 
+  // Add all documentation pages
+  docs.forEach(doc => {
+    sitemap += `
+<url>
+  <loc>${baseUrl}/docs/${doc}</loc>
+  <lastmod>${currentDate}</lastmod>
+</url>`;
+  });
 
-</urlset>`, 'text/xml');
+  sitemap += `
+</urlset>`;
+
+  res.send(sitemap, 'text/xml');
 });
 app.on(404, () => {
   return fs.readFileSync(path.join(__dirname, '../public/404.html'), 'utf8');
 });
-app.error(async(err, req, res) => {
+app.error((err, req, res) => {
   res.send(fs.readFileSync(path.join(__dirname, '../public/500.html'), 'utf8'));
 })
 app.at(port, () => {
