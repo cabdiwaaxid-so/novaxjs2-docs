205ee70148865578fc850888175303a0ca4e64cf
Cabdi Waaxid Siciid <158821136+WhoappRoom@users.noreply.github.com>
2025-07-14 07:08:07 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,33 +31,7 @@ app.get('/docs/:feature', (req, res) => {
   }
 })
 app.get('/sitemap', (req, res) => {
-  const baseUrl = `${req.protocol}://${req.headers.host}`;
-  const currentDate = new Date().toISOString();
-  
-  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<url>
-  <loc>${baseUrl}/</loc>
-  <lastmod>2025-04-24T21:21:19+00:00</lastmod>
-</url>`;
-
-  // Add all documentation pages
-  docs.forEach(doc => {
-    sitemap += `
-<url>
-  <loc>${baseUrl}/docs/${doc}</loc>
-  <lastmod>${currentDate}</lastmod>
-</url>`;
-  });
-
-  sitemap += `
-</urlset>`;
-
-  res.send(sitemap, 'text/xml');
+  app.sendFile('../public/sitemap.xml', 'text/xml', res);
 });
 app.on(404, () => {
   return fs.readFileSync(path.join(__dirname, '../public/404.html'), 'utf8');
