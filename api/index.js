47741ab0dd78612472db51bc65b9ecf09fdb566f
Cabdi Waaxid Siciid <158821136+cabdiwaaxid-so@users.noreply.github.com>
2025-09-22 09:51:45 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -9,39 +9,16 @@ app.setViewEngine('novax', {
   viewsPath: path.join(__dirname, '../public/docs')
 })
 const docs = ['about', 'installation', 'features', 'usage', 'api-reference', 'plugin-system', 'file-uploads', 'cors', 'middleware', 'templating', 'route-middleware', 'responses', 'error-handling', 'example', 'projects', 'contact', 'static-files', 'file-configuration', 'app']
-app.get('/', (req, res) => {
-  app.sendFile(path.join(__dirname, '../public/index.html'), res)
+app.get('/', async (req, res) => {
+  res.send(await app.render('../index'))
 });
-const posts = [
-  {
-    id: "1",
-    title: "Hello Novax!",
-    content: "This is my very first post using Novaxjs2 🚀",
-    author: "Admin",
-    createdAt: "2025-08-21T09:00:00Z"
-  },
-  {
-    id: "2",
-    title: "Getting Started",
-    content: "Novaxjs2 makes building web apps super simple with built-in routing and templates.",
-    author: "Jane Doe",
-    createdAt: "2025-08-21T10:15:00Z"
-  },
-  {
-    id: "3",
-    title: "Why Novax?",
-    content: "Because it’s lightweight, modern, and designed for both frontend and backend flexibility.",
-    author: "John Smith",
-    createdAt: "2025-08-21T11:30:00Z"
-  }
-];
 
 app.get('/docs/:feature', (req, res) => {
   try {
     const target = req.params.feature;
   if(target && docs.includes(target)) {
-    const footer = fs.readFileSync(path.join(__dirname, '../public/footer.html'), 'utf8');
-    const render = app.render(target, {footer: footer, posts: posts });
+    if(target === 'plugin-system') app.minifier = false;
+    const render = app.render(target);
     if(render) {
       return render;
     } else {
@@ -65,6 +42,5 @@ app.error((err, req, res) => {
   console.log(err)
 })
 app.at(port, () => {
-  console.log(`Nova is running on https:localhost:${port}`);
+  console.log(`Novax is running on https:localhost:${port}`);
 });
-console.log(app)
